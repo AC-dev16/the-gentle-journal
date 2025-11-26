f264762faddb090fee9dc39302c1197b719fba08
AC-dev16 <amy_mb@hotmail.co.uk>
2025-11-26 14:48:57 +0000

    Configure Django Allauth for user authentication; update settings and URLs, and set DEBUG to False for production readiness

--- the_gentle_journal/urls.py
+++ the_gentle_journal/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('diary_entries.urls')),
     path('summernote/', include('django_summernote.urls')),
+    path('accounts/', include('allauth.urls')),
 ]

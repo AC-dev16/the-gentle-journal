4da982070ecc287bb13a0c264f50cf87d110d8ba
AC-dev16 <amy_mb@hotmail.co.uk>
2025-11-26 11:47:29 +0000

    Add DiaryEntry model and update admin registration; configure allowed hosts and CSRF trusted origins

--- diary_entries/models.py
+++ diary_entries/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class DiaryEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='diary_entries')
+    location = models.CharField(max_length=100)
+    pain_level =  models.IntegerField()
+    mood_level = models.IntegerField()
+    sleep_hours = models.IntegerField()
+    triggers = models.TextField(blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"Diary Entry by {self.user.username}"
+    
--- the_gentle_journal/settings.py
+++ the_gentle_journal/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -106,6 +106,11 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.herokuapp.com',
+    'http://*.codeinstitute-ide.net',
+    ] 
 
 
 # Internationalization

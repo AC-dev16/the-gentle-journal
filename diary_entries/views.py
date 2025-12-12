965742ce21f3d478309d68bba9cb0412e8d898d3
AC-dev16 <amy_mb@hotmail.co.uk>
2025-12-12 09:26:56 +0000

    Update entries.html to display total entries count in the header

--- diary_entries/views.py
+++ diary_entries/views.py
@@ -59,6 +59,7 @@ class DiaryEntryListView(LoginRequiredMixin, generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['total_entries'] = self.get_queryset().count()
         return context
 
 
